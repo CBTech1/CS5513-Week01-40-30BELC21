208b4048d45ae46f960f5062d6f78764de98c2d3
30BELC21 <11858948-30BELC21@users.noreply.replit.com>
2023-08-22 21:18:34 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,23 @@
+let myhttp = require("http");
+
+let myserver = myhttp.createServer(
+  function( myrequest, myresponse){
+    console.log( myrequest.url);
+
+let mytext;
+
+   if(myrequest.url === "/sup"){
+     mytext = "What is goodie";
+   } else{
+     mytext = "who is you aye?"
+   }
+    
+  myresponse.writeHead( 200, { "Content-Type": "text/plain"});
+
+    myresponse.end(mytext);
+  }
+);
+
+myserver.listen(8080, "0.0.0.0");
+
+console.log("server is initiating");
